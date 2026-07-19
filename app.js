35a44bd558d951b387cbad8a094439b14db5c420
hara-den-ctrl <hara00densetu@gmail.com>
2026-07-20 00:52:25 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+<rect width="512" height="512" rx="110" fill="#147a52"/>
+<path fill="#fff" d="M384 86c-132 16-230 77-247 186-12 79 37 143 111 154 82 12 151-43 160-128 8-74-7-143-24-212Z"/>
+<path d="M128 402c55-100 129-174 245-257M231 314c-18-48-10-89 11-126M305 255c38 5 72-3 98-23" fill="none" stroke="#147a52" stroke-width="27" stroke-linecap="round"/>
+</svg>
